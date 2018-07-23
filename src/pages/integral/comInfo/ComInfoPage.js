bb378b9074f43a795a21faec5a74d9141d7be3f5
longbatian <425774702@qq.com>
2018-07-23 10:31:36 +0800

    723

---
+++ src/pages/integral/comInfo/ComInfoPage.js
@@ -0,0 +1,25 @@
+import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
+import InterfaceUtil from './../../../util/InterfaceUtil';
+
+import './components/comInfo.css';
+
+
+class ComInfoPage extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {}
+    }
+
+    componentDidMount() {
+
+    }
+
+    render() {
+        return <div>
+222
+        </div>
+    }
+}
+
+export default ComInfoPage;
