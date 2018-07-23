3fcefe91ee3268709984c1e966dd35e56a8aee85
longbatian <425774702@qq.com>
2018-07-23 14:32:09 +0800

    723

--- src/pages/integral/comInfo/ComInfoPage.js
+++ src/pages/integral/comInfo/ComInfoPage.js
@@ -17,7 +17,36 @@ class ComInfoPage extends Component {
 
     render() {
         return <div>
-222
+            <div className="cmiBox">
+                <div className="cmiHead">
+                    <img src={require('../../../images/interl/03.png')} alt=""/>
+                    <div className="cmiHeaddiv">
+                        <h2>标题文本文本文本文本文本文本文本文本文本文本文本文本文本文本文本</h2>
+                        <div className="cmiHeaddivcon">
+                            <p>参考价：<span className="hen">￥12.6</span></p>
+                            <p>积分数: <span className="yellow">499999</span></p>
+                            <p>库存数:20</p>
+                        </div>
+                        <div className="cmiHeaddivfot">
+                            <div className="cmiHeaddivconlef">
+                                <span>-</span>
+                                <span>+</span>
+
+                                <input type="text"/>
+                            </div>
+                            <button>立即兑换</button>
+                        </div>
+                        <div className="cmiHeaddivconyijing">
+                            已有 8888 人兑换
+                        </div>
+                    </div>
+                </div>
+                <div className="cmiCon">
+                    <div className="cmiContit">
+                        礼品详情
+                    </div>
+                </div>
+            </div>
         </div>
     }
 }
