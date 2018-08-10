20386c9e47342a2c3dcbf50cad0187c40085297f
longbatian <425774702@qq.com>
2018-08-10 08:21:13 +0800

    809

---
+++ src/pages/balance/components/BalancePage3.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import {Input} from 'antd';
+import './balanceLess.css'
+
+
+class BalancePage1 extends Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return <div className=" ">
+
+        </div>
+    }
+}
+
+export default BalancePage1;
