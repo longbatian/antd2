be8a3bb66bf46cefde737e2cc421ad666cb6ce1a
longbatian <425774702@qq.com>
2018-08-14 14:46:13 +0800

    813-seven

---
+++ src/pages/zhijun/Zhijun.js
@@ -0,0 +1,15 @@
+import React,{Component} from 'react';
+
+import {Link,withRouter} from 'react-router-dom';
+class Zhijun extends Component {
+    constructor(props){
+        super()
+    }
+    render(){
+        return <div>
+
+        </div>
+    }
+}
+
+export default Zhijun;
